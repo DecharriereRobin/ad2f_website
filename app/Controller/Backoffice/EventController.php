dbb03a253264cff9ec325233bd683ae061d82686
[VincentGossey] <treehouseadvisor@gmail.com>
2016-12-09 17:27:45 +0100

    Fix Conflict

--- app/Controller/Backoffice/EventController.php
+++ app/Controller/Backoffice/EventController.php
@@ -40,10 +40,7 @@ class EventController extends \W\Controller\Controller
         $errorClass = [];
         
         if(isset($_POST['createEvent'])){
-<<<<<<< HEAD
-            if(!empty($_POST['title']) && !empty($_POST['content']) && !empty($_POST['date']) && !empty($_POST['category'])){
 
-=======
             $errorMessages = [];
             
             if(empty($_POST['title'])){
@@ -62,24 +59,21 @@ class EventController extends \W\Controller\Controller
             }
             
             if(count($errorMessages)== 0){
->>>>>>> badd70c6b53be96d4262efcf7d44aee1e188e61f
                 $event->insert([
                     'title' => trim($_POST['title']),
                     'content' => trim($_POST['content']),
                     'date' => trim($_POST['date']),
                     'category' => $_POST['category']
-<<<<<<< HEAD
-                ], $id, true);
+                ], true);
 
             $message = "<div class='alert alert-success'>L'évenement a bien été créé.</div>";
             }else{
                 $message = "<div class='alert alert-danger'>L'évenement n'a pas été créé.</div>";
             }
         }
-
         $this->show('eventCreate', ['message'=>$message]);
 
-=======
+
                 ], true);
                 
                 $message = "<div class='alert alert-success'>L'évenement a bien été créé.</div>";
@@ -90,17 +84,12 @@ class EventController extends \W\Controller\Controller
         }
         
         $this->show('backoffice/eventCreate', ['message'=>$message, 'errorMessages' => $errorMessages, 'hasError' => $errorClass]);
-		
->>>>>>> badd70c6b53be96d4262efcf7d44aee1e188e61f
 	}
 
 
 	public function eventEdit($id)
-<<<<<<< HEAD
-	{
-=======
     {
->>>>>>> badd70c6b53be96d4262efcf7d44aee1e188e61f
+
         //$this->allowTo('admin'); // Only Admin User allowed for Back Office function
         $event = new Event();
 
@@ -110,9 +99,6 @@ class EventController extends \W\Controller\Controller
        
         // Check if User filled form
         if(isset($_POST['editEvent'])){
-<<<<<<< HEAD
-            if(!empty($_POST['title']) && !empty($_POST['content']) && !empty($_POST['date'])){
-=======
             
             if(empty($_POST['title'])){
                 $errorMessages[] = 'Le titre doit être complété';
@@ -127,7 +113,6 @@ class EventController extends \W\Controller\Controller
             }
             
             if(!count($errorMessages)){
->>>>>>> badd70c6b53be96d4262efcf7d44aee1e188e61f
 
                 $event->update([
                     'title' => trim($_POST['title']),
@@ -136,22 +121,12 @@ class EventController extends \W\Controller\Controller
                     'category' => $_POST['category']
                 ], $id, true);
 
-<<<<<<< HEAD
-            $message = "<div class='alert alert-success'>L'évenement a bien été modifié.</div>";
-            }else{
-                $message = "<div class='alert alert-danger'>L'évenement n'a pas été modifié.</div>";
-            }
-        }
-
-        $this->show('eventEdit', ['event' => $event->find($id), 'message'=>$message]);
-=======
                 $message = "<div class='alert alert-success'>L'évenement a bien été modifié.</div>";
             } else {
 
                 $message = "<div class='alert alert-danger'>L'évenement n'a pas été modifié.</div>";
             }
         }
->>>>>>> badd70c6b53be96d4262efcf7d44aee1e188e61f
 
         $this->show('backoffice/eventEdit', ['event' => $event->find($id), 'message'=>$message]);
     }
