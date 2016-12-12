2267cfb9c34a91b9c2629f9721678d4a48790259
fredericsoude <fredericsoude59000@gmail.com>
2016-12-12 16:24:57 +0100

    Add files via upload

--- app/Controller/Backoffice/MeetingsController.php
+++ app/Controller/Backoffice/MeetingsController.php
@@ -2,53 +2,60 @@
 
 namespace Controller\Backoffice;
 
-use \W\Controller\Controller;
 use Model\MeetingsModel as Meetings;
+use \W\Controller\Controller;
 
-
-class MeetingsController extends Controller
+class MeetingsController extends \W\Controller\Controller
 {
-
-	public function liste()
-	{
-         //$this->allowTo('admin'); // Only Admin User allowed for Back Office function
+    public function liste()
+	{   
         $meetings = new Meetings();
-		$this->show('backoffice/meetingsList', ['meetings' => $meetings->findAll()]);
+        $message = "";
+        $this->show('backoffice/meetingsList', ['meetings' => $meetings->findAll()]);
 	}
-        
-	
-
+    
     public function create()
 	{
-        $message = "";
         $meetings = new Meetings();
-       
+        $message = "";
         if(isset($_POST['createMeetings'])){
-            if(!empty($_POST['date']) && !empty($_POST['address']) && !empty($_POST['content'])){
-                $meetings->insert([
-                    'date' => $_POST['date'],
-                    'address' => trim($_POST['address']),
-                    'content' => trim($_POST['content']),
-                ],true);
-                
-            $message = "<div class='alert alert-success'>L'évenement a bien été créé dans l'agenda.</div>";
+        if(!empty($_POST['date']) && !empty($_POST['address']) && !empty($_POST['content'])){
+            $meetings->insert([
+            'date' => $_POST['date'],
+            'address' => trim($_POST['address']),
+            'content' => trim($_POST['content']),
+            ],true);
+            $message = "<div class='alert alert-success'>La réunion a bien été créée dans l'agenda.</div>";
             }else{
-                $message = "<div class='alert alert-danger'>L'évenement n'a pas été créé dans l'agenda.</div>";
+            $message = "<div class='alert alert-danger'>La réunion n'a pas été créée dans l'agenda.</div>";
             }
         }
         $this->show('backoffice/meetingsCreate', ['message'=>$message]);
-		
 	}
-
+    
     public function delete($id)
 	{
-        //$this->allowTo('admin'); // Only Admin User allowed for Back Office function and delete from agenda
+        //$this->allowTo('admin'); // Only Admin User allowed for Back Office function and delete from agenda 
+            $meetings = new Meetings();
+            $meetings->delete($id);
+            $this->redirectToRoute('backoffice_MeetingsList');
+    }
+    public function edit($id)
+    {
+        //$this->allowTo('admin'); // Only Admin User allowed for Back Office function 
         $meetings = new Meetings();
-        $meetings->delete($id);  
-        $this->show('backoffice/meetingsList', ['meetings' => $meetings->findAll()]);
-        
-	}
-    
-	}
-    
-
+        $message = "";
+            if(isset($_POST['editMeetings'])){
+            if(!empty($_POST['date']) && !empty($_POST['address']) && !empty($_POST['content'])){
+                $meetings->update([
+                    'date' => trim($_POST['date']),
+                    'address' => trim($_POST['address']),
+                    'content' => trim($_POST['content']),
+                    ],$id, true);
+                
+            $this->redirectToRoute('backoffice_MeetingsList');
+             } 
+        }
+        $this->show('backoffice/meetingsEdit', ['meeting' => $meetings->find($id)]);
+    }
+}
