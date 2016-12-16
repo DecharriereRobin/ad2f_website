f04bdeb58ac12e90f058b48813b5965eeff0f312
elrka <vincentjenni@gmail.com>
2016-12-16 17:23:51 +0100

    vendredi soir

---
+++ public/association_presentation.php
@@ -0,0 +1,60 @@
+<?php
+  include 'header_sm_reso.php';
+?>
+
+<!-- OPEN MAIN-->
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <h2 class="page_title">
+              <hr>
+            L'ASSOCIATION
+          </h2>
+        </div>
+        <div>
+          <p class="paragraphe_text">
+            Née en 2014, elle regroupe une soixantaine d’adhérents. Son but: créer du lien entre les habitants et animer ce coin de Lille-sud, appelé aussi Filbertville. <br />
+            Le carnaval de printemps, la bourse aux jouets, la fête des voisins, un après-midi jeux en plein air... et bien sûr le vide-grenier, l’association a déjà pas mal d’initiatives à son actif et encore beaucoup de projets en tête.
+          </p>
+        </div>
+      </div> <!-- Close Row -->
+
+      <div class="row">
+        <div class="col-xs-push-2 col-xs-8 div_newsletter">
+          <div class="inside_div_newsletter">
+            <h2 class="title_div_newlestter">NEWSLETTER</h2>
+              <div class="form-group">
+                <form action="">
+                  <input type="email" placeholder="your.email@email.com" name="newsletter" class="form-control newsletter_input">
+                  <button type="submit" name="submit_newsletter" class="submit_newsletter btn btn-default">S' inscrire</button>
+                </form>
+              </div>
+          </div>
+        </div>
+        
+        <div class="col-xs-12">
+          <h2 class="title_div_facebook">
+              REJOIGNEZ NOUS SUR FACEBOOK
+          </h2>
+            <div class="col-lxs-10 div_img_facebook">
+              <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAssociationdes2faubourgs%2F%3Fref%3Dpage_internal&tabs=timeline&width=500px&height=150px&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="400px" height="150px" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+            </div>
+        </div>
+     
+
+
+
+
+      </div> <!-- Close Row -->
+
+
+
+
+    </div> <!-- Close Container -->
+  </main>
+<!-- CLOSE MAIN-->
+
+<?php
+  include 'footer_sm_reso.php';
+?>
